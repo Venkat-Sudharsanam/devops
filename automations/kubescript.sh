a783ae3be1789c32606b1ec848a7ee3d6820dc6b
Venkat-Sudharsanam <vishal.ronaldo93@gmail.com>
2016-10-22 00:03:10 +0530

    Update kubescript.sh

--- automations/kubescript.sh
+++ automations/kubescript.sh
@@ -22,27 +22,24 @@ apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
 clear
 echo "Master setup has been finished."
 echo "Setting up minions"
-mkdir -p /kubernetes
-cd /kubernetes
-echo "Please provide a name for the script to run in your minions"
-read $minion
-touch $minion
-tee $minion <<-'DONE'
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+touch minion.sh
+tee minion.sh <<-'DONE'
+#!/bin/bash
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+sudo cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update -y
-apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+sudo apt-get update -y
+sudo apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
 DONE
 clear
 echo "Enter the number of minions nodes"
-read $minionnumb
+read minionnumb
 for ((i=1; i<=$minionnumb; i++))
 do
 echo "Please give the $i minion ip:"
 read minionip
-ssh root@$minionip /kubernetes/$minion.sh
+ssh root@$minionip ./kube/minion.sh
 echo "Prerequisites isntalled  $i master."
 done
 echo "All minions have been configured."
