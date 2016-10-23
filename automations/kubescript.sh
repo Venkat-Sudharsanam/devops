f671090df2469879b63da626abeb0c9b6581b199
Venkat-Sudharsanam <vishal.ronaldo93@gmail.com>
2016-10-23 22:41:19 +0530

    Update kubescript.sh

--- automations/kubescript.sh
+++ automations/kubescript.sh
@@ -25,7 +25,6 @@ echo "Master setup has been finished."
 echo "Setting up minions"
 touch minion.sh
 tee minion.sh <<-'DONE'
-#!/bin/bash
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 sudo cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
